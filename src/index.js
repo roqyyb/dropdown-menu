5fa6639d18bc6a2c9d8253e1497617f802a774eb
roqyyb <125754105+roqyyb@users.noreply.github.com>
2025-08-28 08:12:10 -0500

    Design and implement the dropdown component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import _ from "lodash";
+import "./style.css";
+
+const content = [
+  {
+    id: 1,
+    text: "Link 1",
+  },
+  {
+    id: 2,
+    text: "Link 2",
+  },
+  {
+    id: 3,
+    text: "Link 3",
+  },
+];
+
+function DropdownMenu(text = "Dropdown Menu", content) {
+  const element = document.createElement("div");
+  element.classList.add("dropdown");
+
+  //  button
+  const dropdownBtn = document.createElement("button");
+  dropdownBtn.textContent = text;
+  dropdownBtn.classList.add("dropdown-btn");
+
+  element.appendChild(dropdownBtn);
+
+  // content
+  const dropdownContent = document.createElement("div");
+  dropdownContent.classList.add("dropdown-content");
+  content.forEach((item) => {
+    const { id, text } = item;
+    const dropItem = document.createElement("a");
+    dropItem.classList.add("dropdown-item");
+    dropItem.setAttribute("id", id);
+    dropItem.textContent = text;
+
+    dropdownContent.appendChild(dropItem);
+  });
+
+  //final stuff
+  element.appendChild(dropdownContent);
+
+  return element;
+}
 
 function App() {
   const app = document.createElement("div");
 
-  app.innerHTML = _.join(["Dropdown", "Menu"], " ");
+  app.appendChild(DropdownMenu("Click Me", content));
 
   return app;
 }
